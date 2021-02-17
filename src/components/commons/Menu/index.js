245a212253bde0a0e20aa4be4eca0ff62454f07e
Paulo Cesar Cassin Filho <pccassin@MacBook-Air-de-Cassin.local>
2021-02-17 08:06:04 -0300

    adicionando home que faltou no commit

--- src/components/commons/Menu/index.js
+++ src/components/commons/Menu/index.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { Logo } from "../../../theme/Logo";
 import { MenuWrapper } from "./styles/MenuWrapper";
 
 export default function Menu() {
   return (
     <MenuWrapper>
-      <MenuWrapper.LeftSide>Logo Area</MenuWrapper.LeftSide>
+      <MenuWrapper.LeftSide> <Logo /> </MenuWrapper.LeftSide>
       <MenuWrapper.CentralSide>Links Area</MenuWrapper.CentralSide>
       <MenuWrapper.RightSide>Buttons Area</MenuWrapper.RightSide>
     </MenuWrapper>

dd77bc348e03c1199adeb9bdb6195f60eca7a9fb
Paulo Cesar Cassin Filho <pccassin@MacBook-Air-de-Cassin.local>
2021-02-17 11:03:39 -0300

    Fim aula 4 - styled components theme

--- src/components/commons/Menu/index.js
+++ src/components/commons/Menu/index.js
@@ -1,13 +1,47 @@
 import React from "react";
 import { Logo } from "../../../theme/Logo";
+import { Button } from "../Button";
 import { MenuWrapper } from "./styles/MenuWrapper";
 
 export default function Menu() {
+
+  const links = [
+    {
+      texto: 'Home',
+      url: '/'
+    },
+    {
+      texto: 'Perguntas frequentes',
+      url: '/faq'
+    },
+    {
+      texto: 'Sobre',
+      url: '/sobre'
+    }
+  ];
+
   return (
     <MenuWrapper>
       <MenuWrapper.LeftSide> <Logo /> </MenuWrapper.LeftSide>
-      <MenuWrapper.CentralSide>Links Area</MenuWrapper.CentralSide>
-      <MenuWrapper.RightSide>Buttons Area</MenuWrapper.RightSide>
+      <MenuWrapper.CentralSide>
+        {links.map((link) => {
+          return (
+            <li key={link.url}>
+              <a href={link.url}>
+                {link.texto}
+              </a>
+            </li>
+          )
+        })}
+      </MenuWrapper.CentralSide>
+      <MenuWrapper.RightSide>
+        <Button ghost variant="secondary.main">
+          Entrar
+        </Button>
+        <Button variant="primary.main">
+          Cadastrar
+        </Button>
+      </MenuWrapper.RightSide>
     </MenuWrapper>
   );
 }
